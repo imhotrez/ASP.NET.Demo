3057f361dc8eaffb006b9bb1050a174a5110f1e2
Евгений Бородин <imhotrez@outlook.com>
2021-03-11 20:50:51 +0500

    Added Demo.Models project containing models. Implementation of a generic response model.

---
+++ src/Demo.WebAPI/Demo.Models/Dto/Error.cs
@@ -0,0 +1,6 @@
+﻿namespace Demo.Models.Dto {
+    public class Error {
+        public string ErrorMessage { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
---
+++ src/Demo.WebAPI/Demo.Models/Dto/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Models.Dto {
+    public class Response<T> {
+        public Response(T data, Error error = null) {
+            Data = data;
+            Error = error;
+        }
+
+        public T Data { get; }
+        public Error Error { get; }
+    }
+}
