a3fafffe3697ef7882818661900aae69e8663b35
Евгений Бородин <imhotrez@outlook.com>
2021-05-23 20:01:47 +0500

    Implemented sequential downloads of previews and originals of photos

---
+++ src/Demo.WebAPI/Demo.SPA/Models/ImageData.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo.SPA.Models {
+    public class ImageData {
+        public long FileId { get; set; }
+        public string FileName { get; set; }
+        public string Original { get; set; }
+        public string Preview { get; set; }
+    }
+}
--- src/Demo.WebAPI/Demo.WebAPI/Services/BusinessLogic/ImageTransport.cs
+++ src/Demo.WebAPI/Demo.WebAPI/Services/BusinessLogic/ImageTransport.cs
@@ -12,27 +12,29 @@ using BytesContentDownload = Demo.gRPC.SPA.FileTransport.BytesContentDownload;
 namespace Demo.WebAPI.Services.BusinessLogic {
     public class ImageTransport : ImageTransportService.ImageTransportServiceBase {
         private readonly FileProviderService fileProviderService;
-        private readonly DemoContext context;
+        private readonly DemoContext dbContext;
 
-        public ImageTransport(FileProviderService fileProviderService, DemoContext context) {
+        public ImageTransport(FileProviderService fileProviderService, DemoContext dbContext) {
             this.fileProviderService = fileProviderService;
-            this.context = context;
+            this.dbContext = dbContext;
         }
 
         public override async Task FileDownload(UserInfo request,
             IServerStreamWriter<BytesContentDownload> responseStream,
             ServerCallContext context) {
-            var imagesIds = await this.context.Users
+            var imagesIds = await this.dbContext.Users
                 .Include(u => u.ImageMetadatas)
                 .SelectMany(u => u.ImageMetadatas.Select(x => x.Id))
                 .ToArrayAsync(CancellationToken.None);
 
+            var imageType = request.IsPreview ? ImageType.Preview : ImageType.Original;
             foreach (var imageId in imagesIds) {
-                var result = await fileProviderService.DownloadFile(imageId, ImageType.Preview, CancellationToken.None);
+                var result = await fileProviderService.DownloadFile(imageId, imageType, CancellationToken.None);
                 await responseStream.WriteAsync(new BytesContentDownload {
                     Content = ByteString.CopyFrom(result.Body),
                     FileName = result.Name,
-                    IsPreview = false
+                    IsPreview = request.IsPreview,
+                    FileId = imageId
                 });
             }
         }
